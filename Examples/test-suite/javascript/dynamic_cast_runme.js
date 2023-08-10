b858a656252b5c4288c76ac0627b798413118540
Momtchil Momtchev <momtchil@momtchev.com>
2023-08-10 11:10:09 +0200

    avoid instanceof with jsc

--- Examples/test-suite/javascript/dynamic_cast_runme.js
+++ Examples/test-suite/javascript/dynamic_cast_runme.js
@@ -1,9 +1,7 @@
 var dynamic_cast = require("dynamic_cast");
 
 var f = new dynamic_cast.Foo();
-if (!(f instanceof dynamic_cast.Foo)) throw new Error;
 var b = new dynamic_cast.Bar();
-if (!(b instanceof dynamic_cast.Bar)) throw new Error;
 
 var x = /* await */(f.blah());
 var y = /* await */(b.blah());
