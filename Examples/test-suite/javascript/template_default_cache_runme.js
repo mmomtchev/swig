2b69b0542cc3ac53c49be6304268a2874d2160f4
Momtchil Momtchev <momtchil@momtchev.com>
2023-09-07 17:35:14 +0200

    ignore process for TS

--- Examples/test-suite/javascript/template_default_cache_runme.js
+++ Examples/test-suite/javascript/template_default_cache_runme.js
@@ -3,6 +3,7 @@ var template_default_cache = require("template_default_cache");
 var ap = /* await */(template_default_cache.get_mp_a());
 var bp = /* await */(template_default_cache.get_mp_b());
 
+/* @ts-ignore */
 if (typeof process !== 'undefined' && typeof process.env !== undefined && process.env.ENGINE === 'napi') {
   // This works correctly only in Node-API
   if (!(ap instanceof template_default_cache.AModelPtr)) {
