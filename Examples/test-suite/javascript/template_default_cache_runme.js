79eca34953d287343ae48fbfa2c7c15cc4935147
Momtchil Momtchev <momtchil@momtchev.com>
2023-09-07 09:43:23 +0200

    actual error detected by TypeScript

--- Examples/test-suite/javascript/template_default_cache_runme.js
+++ Examples/test-suite/javascript/template_default_cache_runme.js
@@ -3,9 +3,9 @@ var template_default_cache = require("template_default_cache");
 var ap = /* await */(template_default_cache.get_mp_a());
 var bp = /* await */(template_default_cache.get_mp_b());
 
-if (!ap instanceof template_default_cache.AModelPtr) {
+if (!(ap instanceof template_default_cache.AModelPtr)) {
     throw new Error("get_mp_a fail");
 }
-if (!bp instanceof template_default_cache.BModelPtr) {
+if (!(bp instanceof template_default_cache.BModelPtr)) {
     throw new Error("get_mp_b fail");
 }
