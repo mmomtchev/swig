3aba1f2833b9e0251241c73bb87105da883bd7c1
Momtchil Momtchev <momtchil@momtchev.com>
2024-01-25 14:35:53 +0100

    add a runme for this test

---
+++ Examples/test-suite/javascript/exception_memory_leak_runme.js
@@ -0,0 +1,35 @@
+var exception_memory_leak = require('exception_memory_leak');
+
+var { Foo } = exception_memory_leak;
+
+// This test works correctly only in Node-API
+if (typeof print === 'undefined' && typeof exception_memory_leak.Foo.prototype.equals === 'undefined') {
+
+  var a = new Foo;
+  if (Foo.get_count() != 1) throw new Error("Should have 1 Foo objects");
+  var b = new Foo;
+  if (Foo.get_count() != 2) throw new Error("Should have 2 Foo objects");
+
+  // Normal behaviour
+  exception_memory_leak.trigger_internal_swig_exception("no problem", a);
+  if (Foo.get_count() != 2) throw new Error("Should have 2 Foo objects");
+  if (Foo.get_freearg_count() != 1) throw new Error("freearg should have been used once");
+
+  // SWIG exception triggered and handled (return new object case).
+  var fail = false;
+  try {
+    exception_memory_leak.trigger_internal_swig_exception("null", b);
+    fail = true;
+  } catch { }
+  if (fail) throw new Error("Expected an exception");
+
+  if (Foo.get_count() != 2) throw new Error("Should have 2 Foo objects");
+  if (Foo.get_freearg_count() != 2) throw new Error("freearg should have been used twice");
+
+  // SWIG exception triggered and handled (return by value case).
+  try {
+    exception_memory_leak.trigger_internal_swig_exception("null");
+    fail = true;
+  } catch { }
+  if (fail) throw new Error("Expected an exception");
+}
