9225a12fb938f3d76c6a57e18838fd279155f33d
Momtchil Momtchev <momtchil@momtchev.com>
2024-03-22 18:03:51 +0100

    test vectors and maps for shared_ptr

--- Examples/test-suite/javascript/cpp11_shared_ptr_upcast_runme.js
+++ Examples/test-suite/javascript/cpp11_shared_ptr_upcast_runme.js
@@ -13,29 +13,29 @@ function swig_assert_equal_simple(expected, got) {
 
 // non-overloaded
 swig_assert_equal_simple(7, cpp11_shared_ptr_upcast.derived_num1(new Derived(7)));
-//swig_assert_equal_simple(7, cpp11_shared_ptr_upcast.derived_num2([new Derived(7)]));
-//swig_assert_equal_simple(7, cpp11_shared_ptr_upcast.derived_num3({ 0: new Derived(7) }));
+swig_assert_equal_simple(7, cpp11_shared_ptr_upcast.derived_num2([new Derived(7)]));
+swig_assert_equal_simple(7, cpp11_shared_ptr_upcast.derived_num3({ 0: new Derived(7) }));
 
 swig_assert_equal_simple(-1, cpp11_shared_ptr_upcast.base_num1(new Derived(7)));
-//swig_assert_equal_simple(-1, cpp11_shared_ptr_upcast.base_num2([new Derived(7)]));
-//swig_assert_equal_simple(-1, cpp11_shared_ptr_upcast.base_num3({ 0: new Derived(7) }));
+swig_assert_equal_simple(-1, cpp11_shared_ptr_upcast.base_num2([new Derived(7)]));
+swig_assert_equal_simple(-1, cpp11_shared_ptr_upcast.base_num3({ 0: new Derived(7) }));
 
 swig_assert_equal_simple(999, cpp11_shared_ptr_upcast.derived_num1(null));
-// not working: swig_assert_equal_simple(999, derived_num2([None]))
-// not working: swig_assert_equal_simple(999, derived_num3({0: None}))
+swig_assert_equal_simple(999, cpp11_shared_ptr_upcast.derived_num2([null]));
+swig_assert_equal_simple(999, cpp11_shared_ptr_upcast.derived_num3({ 0: null }));
 
 swig_assert_equal_simple(999, cpp11_shared_ptr_upcast.base_num1(null));
-// not working: swig_assert_equal_simple(999, base_num2([None]))
-// not working: swig_assert_equal_simple(999, base_num3({0: None}))
+swig_assert_equal_simple(999, cpp11_shared_ptr_upcast.base_num2([null]));
+swig_assert_equal_simple(999, cpp11_shared_ptr_upcast.base_num3({ 0: null }));
 
 // overloaded
 swig_assert_equal_simple(7, cpp11_shared_ptr_upcast.derived_num(new Derived(7)));
-//swig_assert_equal_simple(7, cpp11_shared_ptr_upcast.derived_num([new Derived(7)]));
-//swig_assert_equal_simple(7, cpp11_shared_ptr_upcast.derived_num({ 0: new Derived(7) }));
+swig_assert_equal_simple(7, cpp11_shared_ptr_upcast.derived_num([new Derived(7)]));
+swig_assert_equal_simple(7, cpp11_shared_ptr_upcast.derived_num({ 0: new Derived(7) }));
 
 swig_assert_equal_simple(-1, cpp11_shared_ptr_upcast.base_num(new Derived(7)));
-//swig_assert_equal_simple(-1, cpp11_shared_ptr_upcast.base_num([new Derived(7)]));
-//swig_assert_equal_simple(-1, cpp11_shared_ptr_upcast.base_num({ 0: new Derived(7) }));
+swig_assert_equal_simple(-1, cpp11_shared_ptr_upcast.base_num([new Derived(7)]));
+swig_assert_equal_simple(-1, cpp11_shared_ptr_upcast.base_num({ 0: new Derived(7) }));
 
 // ptr to shared_ptr
 //swig_assert_equal_simple(7, cpp11_shared_ptr_upcast.derived2_num1(new Derived2(7)));
