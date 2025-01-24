523ef73e94aa54b0e9bdaa9f825a5c486937404b
Momtchil Momtchev <momtchil@momtchev.com>
2025-01-24 16:33:43 +0100

    add an LLDB script

---
+++ Tools/lldb.py
@@ -0,0 +1,34 @@
+# User-defined commands for easier debugging of SWIG in lldb
+#
+# This file can be "included" using:
+# > command script import ..path../swig/Tools/lldb.py
+
+import shlex
+
+def swigprint(debugger, command, result, internal_dict):
+        """
+        Displays any SWIG DOH object
+        Usage: swigprint swigobject [hashexpandcount]
+        swigobject      - The object to display.
+        hashexpandcount - Number of nested Hash types to expand (default is 1). See Swig_set_max_hash_expand() to change default.
+        """
+        args = shlex.split(command)
+        var = args[0]
+        expand_count = -1 if args.__len__() < 2 else int(args[1])
+        debugger.HandleCommand(f'p Swig_to_string({var}, {expand_count}))')
+
+def locswigprint(debugger, command, result, internal_dict):
+        """
+        Displays any SWIG DOH object prefixed with file and line location
+        Usage: locswigprint swigobject [hashexpandcount]
+        swigobject      - The object to display.
+        hashexpandcount - Number of nested Hash types to expand (default is 1). See Swig_set_max_hash_expand() to change default.
+        """
+        args = shlex.split(command)
+        var = args[0]
+        expand_count = -1 if args.__len__() < 2 else int(args[1])
+        debugger.HandleCommand(f'p Swig_to_string_with_location({var}, {expand_count}))')
+
+def __lldb_init_module(debugger, internal_dict):
+        debugger.HandleCommand("command script add -h \"Displays any SWIG DOH object\" -f " + __name__ + ".swigprint swigprint")
+        debugger.HandleCommand("command script add -h \"Displays any SWIG DOH object prefixed with file and line location\" -f " + __name__ + ".locswinprint locswigprint")
