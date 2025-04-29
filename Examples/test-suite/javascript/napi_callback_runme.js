5c2597dab7b1467878fd65002955b58daed6031e
Momtchil Momtchev <momtchil@momtchev.com>
2025-04-29 20:21:17 +0200

    test async callbacks

--- Examples/test-suite/javascript/napi_callback_runme.js
+++ Examples/test-suite/javascript/napi_callback_runme.js
@@ -1,57 +1,134 @@
 const napi_callback = require('napi_callback');
 
-// Nominal
-const r1 = /* await */(napi_callback.GiveMeFive((pass, name) => {
-  if (pass !== 420) throw new Error('expected 420 as pass');
-  if (typeof name !== 'string') throw new Error('expected a string as name');
-  return 'sent from JS ' + name;
-}));
-if (r1 !== 'received from JS: sent from JS with cheese')
-  throw new Error(`not the expected value, received "${r1}"`);
+// TODO: For some reason my method of handling rejections
+// trips the unhandledRejection detection of Node.js
+// Investigate further
+process.on('unhandledRejection', (e) => {
+  console.log('warning, unhandled rejection', e)
+});
 
-const r2 = /* await */(napi_callback.GiveMeFive_C((pass, name) => {
-  if (pass !== 420) throw new Error('expected 420 as pass');
-  if (typeof name !== 'string') throw new Error('expected a string as name');
-  return 'sent from JS ' + name;
-}));
-if (r2 !== 'received from JS: sent from JS with extra cheese')
-  throw new Error(`not the expected value, received "${r2}"`);
+/**
+ * Sync callback
+ */
+{
+  // Nominal
+  const r1 = /* await */(napi_callback.GiveMeFive((pass, name) => {
+    if (pass !== 420) throw new Error('expected 420 as pass');
+    if (typeof name !== 'string') throw new Error('expected a string as name');
+    return 'sent from JS ' + name;
+  }));
+  if (r1 !== 'received from JS: sent from JS with cheese')
+    throw new Error(`not the expected value, received "${r1}"`);
 
-let called = false;
-/* await */(napi_callback.JustCall(() => {
-  called = true;
-}));
-if (!called)
-  throw new Error('did not call');
+  const r2 = /* await */(napi_callback.GiveMeFive_C((pass, name) => {
+    if (pass !== 420) throw new Error('expected 420 as pass');
+    if (typeof name !== 'string') throw new Error('expected a string as name');
+    return 'sent from JS ' + name;
+  }));
+  if (r2 !== 'received from JS: sent from JS with extra cheese')
+    throw new Error(`not the expected value, received "${r2}"`);
 
-// Exception
-let first = false;
-try {
-  /* await */(napi_callback.GiveMeFive(() => {
-  throw new Error('First');
-}));
-} catch (e) {
-  if (e.message.match(/First/))
-    first = true;
-}
-if (!first) throw new Error('First exception not correctly propagated');
-
-let second = false;
-try {
-  /* await */(napi_callback.GiveMeFive_C(() => {
-  throw new Error('Second');
-}));
-} catch (e) {
-  if (e.message.match(/Second/))
-    second = true;
-}
-if (!second) throw new Error('Second exception not correctly propagated');
-
-let third = false;
-try {
+  let called = false;
   /* await */(napi_callback.JustCall(() => {
-  called = true;
-}));
-} catch (e) {
+    called = true;
+  }));
+  if (!called)
+    throw new Error('did not call');
+
+  // Exception
+  let first = false;
+  try {
+    /* await */(napi_callback.GiveMeFive(() => {
+      throw new Error('First');
+  }));
+  } catch (e) {
+    if (e.message.match(/First/))
+      first = true;
+  }
+  if (!first) throw new Error('First exception not correctly propagated');
+
+  let second = false;
+  try {
+    /* await */(napi_callback.GiveMeFive_C(() => {
+      throw new Error('Second');
+  }));
+  } catch (e) {
+    if (e.message.match(/Second/))
+      second = true;
+  }
+  if (!second) throw new Error('Second exception not correctly propagated');
+
+  let third = false;
+  try {
+    /* await */(napi_callback.JustCall(() => {
+      throw new Error('Third');
+    }));
+  } catch (e) {
+    if (e.message.match(/Third/))
+      third = true;
+  }
+  if (!third) throw new Error('Third exception not correctly propagated');
+}
+
+/**
+ * Async callback (only in async mode)
+ */
+{
+  // Nominal
+  const r1 = /* await */(napi_callback.GiveMeFive(/* async */(pass, name) => {
+    if (pass !== 420) throw new Error('expected 420 as pass');
+    if (typeof name !== 'string') throw new Error('expected a string as name');
+    return 'sent from JS ' + name;
+  }));
+  if (r1 !== 'received from JS: sent from JS with cheese')
+    throw new Error(`not the expected value, received "${r1}"`);
+
+  const r2 = /* await */(napi_callback.GiveMeFive_C(/* async */(pass, name) => {
+    if (pass !== 420) throw new Error('expected 420 as pass');
+    if (typeof name !== 'string') throw new Error('expected a string as name');
+    return 'sent from JS ' + name;
+  }));
+  if (r2 !== 'received from JS: sent from JS with extra cheese')
+    throw new Error(`not the expected value, received "${r2}"`);
+
+  let called = false;
+  /* await */(napi_callback.JustCall(/* async */() => {
+    called = true;
+  }));
+  if (!called)
+    throw new Error('did not call');
+
+  // Exception
+  let first = false;
+  try {
+    /* await */(napi_callback.GiveMeFive(/* async */() => {
+      throw new Error('First');
+    }));
+  } catch (e) {
+    if (e.message.match(/First/))
+      first = true;
+  }
+  if (!first) throw new Error('First exception not correctly propagated');
+
+  let second = false;
+  try {
+    /* await */(napi_callback.GiveMeFive_C(/* async */() => {
+      throw new Error('Second');
+    }));
+  } catch (e) {
+    if (e.message.match(/Second/))
+      second = true;
+  }
+  if (!second) throw new Error('Second exception not correctly propagated');
+
+  let third = false;
+  try {
+    /* await */(napi_callback.JustCall(/* async */() => {
+      throw new Error('Third');
+    }));
+  } catch (e) {
+    if (e.message.match(/Third/))
+      third = true;
+  }
   if (!third) throw new Error('Third exception not correctly propagated');
 }
