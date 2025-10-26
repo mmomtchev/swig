e95a18260b96f6da469c62037768a05ec0eb43dc
Momtchil Momtchev <momtchil@momtchev.com>
2025-10-26 13:32:42 +0100

    ensure the test fails if the argument is any

--- Examples/test-suite/javascript/enums_runme.js
+++ Examples/test-suite/javascript/enums_runme.js
@@ -5,6 +5,19 @@ var enums = require("enums");
 /* await */(enums.bar3(enums.ABCDE));
 /* await */(enums.bar1(enums.CSP_ITERATION_BWD));
 
+let ok = false
+try {
+  // @ts-expect-error ensure that argument is not any
+  /* await */(enums.bar1('test'))
+} catch {
+  ok = true
+}
+if (!ok) throw new Error;
+
+let x = enums.FGHJI
+// @ts-expect-error ensure the variable is not any
+x = 'invalid'
+
 if (enums.enumInstance != 2) {
     throw new Error;
 }
