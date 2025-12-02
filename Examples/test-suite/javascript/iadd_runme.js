e2acd4c9e5d2ef3e97e546723fee6f396b5172a7
Momtchil Momtchev <momtchil@momtchev.com>
2025-12-03 00:21:47 +0100

    fix more TypeScript types for C++ references and pointers (#136)
    * fix another batch of incorrect types for pointers and references
    
    * do not reuse variables with different types

--- Examples/test-suite/javascript/iadd_runme.js
+++ Examples/test-suite/javascript/iadd_runme.js
@@ -5,5 +5,14 @@ var f = new iadd.Foo();
 f.AsA.x = 3;
 /* await */(f.AsA.addto(f.AsA));
 
+var pass = false;
+try {
+  // @ts-expect-error
+  f.AsA.addto('invalid');
+} catch (e) {
+  pass = true;
+}
+if (!pass) throw new Error();
+
 if (f.AsA.x != 6)
     throw new Error;
