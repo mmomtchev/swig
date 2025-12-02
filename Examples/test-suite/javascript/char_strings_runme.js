1129ff136b713504b80b234274999af20028f19d
Momtchil Momtchev <momtchil@momtchev.com>
2025-12-02 22:10:59 +0100

    add the test

--- Examples/test-suite/javascript/char_strings_runme.js
+++ Examples/test-suite/javascript/char_strings_runme.js
@@ -6,6 +6,25 @@ var assertIsEqual = function(expected, actual) {
   }
 };
 
+var assertThrows = function(fn) {
+  var pass = false;
+
+  try {
+    fn();
+  } catch {
+    pass = true;
+  }
+
+  if (!pass) throw new Error('did not throw');
+}
+
 assertIsEqual("hi there", /* await */(char_strings.CharPingPong("hi there")));
 assertIsEqual("hi there", /* await */(char_strings.CharArrayPingPong("hi there")));
 assertIsEqual("hi there", /* await */(char_strings.CharArrayDimsPingPong("hi there")));
+
+// @ts-expect-error
+assertThrows(() => (/* await */(char_strings.CharPingPong(42))));
+// @ts-expect-error
+assertThrows(() => (/* await */(char_strings.CharArrayPingPong(42))));
+// @ts-expect-error
+assertThrows(() => (/* await */(char_strings.CharArrayDimsPingPong(42))));
