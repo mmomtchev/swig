e50e608d221fbeef4570123a43d60d063762554a
Momtchil Momtchev <momtchil@momtchev.com>
2025-12-03 13:56:41 +0100

    fix incorrect test

--- Examples/test-suite/javascript/li_std_pair_using_runme.js
+++ Examples/test-suite/javascript/li_std_pair_using_runme.js
@@ -4,6 +4,6 @@ var one = new li_std_pair_using.StringStringPair("one", "numero uno");
 var two = new li_std_pair_using.StringIntPair("two", 2);
 
 var tuple = /* await */(li_std_pair_using.bounce(one));
-if (typeof tuple !== 'object' && tuple.first !== one.first || tuple.second !== one.second) {
+if (typeof tuple !== 'object' || tuple.first !== one.first || tuple.second !== one.second) {
     throw new Error;
 }
