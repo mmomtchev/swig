5245a465489c5adaa4b4fbbd275b9bc3096c46dc
Momtchil Momtchev <momtchil@momtchev.com>
2025-12-08 19:25:14 +0100

    final var reuse

--- Examples/test-suite/javascript/cpp11_move_typemaps_runme.js
+++ Examples/test-suite/javascript/cpp11_move_typemaps_runme.js
@@ -8,17 +8,17 @@ var mo = new cpp11_move_typemaps.MoveOnly(111);
 /* await */(cpp11_move_typemaps.Counter.check_counts(1, 0, 0, 1, 0, 2));
 
 /* await */(cpp11_move_typemaps.Counter.reset_counts());
-mo = new cpp11_move_typemaps.MovableCopyable(111);
+var mo2 = new cpp11_move_typemaps.MovableCopyable(111);
 /* await */(cpp11_move_typemaps.Counter.check_counts(1, 0, 0, 0, 0, 0));
-/* await */(cpp11_move_typemaps.MovableCopyable.take(mo));
+/* await */(cpp11_move_typemaps.MovableCopyable.take(mo2));
 /* await */(cpp11_move_typemaps.Counter.check_counts(1, 0, 0, 1, 0, 2));
 /* await */(cpp11_move_typemaps.Counter.check_counts(1, 0, 0, 1, 0, 2));
 
-mo = new cpp11_move_typemaps.MoveOnly(222);
-/* await */(cpp11_move_typemaps.MoveOnly.take(mo));
+var mo3 = new cpp11_move_typemaps.MoveOnly(222);
+/* await */(cpp11_move_typemaps.MoveOnly.take(mo3));
 var exception_thrown = false;
 try {
-  /* await */(cpp11_move_typemaps.MoveOnly.take(mo));
+  /* await */(cpp11_move_typemaps.MoveOnly.take(mo3));
 } catch (e) {
   if (!e.message.includes("cannot release ownership as memory is not owned"))
     throw new Error("incorrect exception message:" + e.message);
@@ -31,9 +31,9 @@ if (!exception_thrown)
 /* await */(cpp11_move_typemaps.Counter.reset_counts());
 var imt = new cpp11_move_typemaps.InstanceMethodsTester();
 
-mo = new cpp11_move_typemaps.MoveOnly(333);
+var mo4 = new cpp11_move_typemaps.MoveOnly(333);
 /* await */(cpp11_move_typemaps.Counter.check_counts(1, 0, 0, 0, 0, 0));
-/* await */(imt.instance_take_move_only(mo));
+/* await */(imt.instance_take_move_only(mo4));
 /* await */(cpp11_move_typemaps.Counter.check_counts(1, 0, 0, 1, 0, 2));
 //delete mo;
 
