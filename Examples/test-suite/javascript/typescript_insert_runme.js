eeea7b1ddd1749197e01b80e84531d4ed5a81810
Momtchil Momtchev <momtchil@momtchev.com>
2026-01-09 18:24:39 +0100

    async compatibility

--- Examples/test-suite/javascript/typescript_insert_runme.js
+++ Examples/test-suite/javascript/typescript_insert_runme.js
@@ -1,13 +1,13 @@
 var typescript_insert = require('typescript_insert');
 
 var a = 2; //  @ts-replace var a: typescript_insert.Custom = 2;
-var r = typescript_insert.add(a, 3);
+var r = /* await */(typescript_insert.add(a, 3));
 if (r !== 5) throw new Error;
 
 var pass = false;
 try {
   // @ts-expect-error
-  typescript_insert.add('a', 'b');
+  /* await */(typescript_insert.add('a', 'b'));
 } catch {
   pass = true;
 }
