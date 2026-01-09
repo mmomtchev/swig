cd285c127bb34a8b6853158fcbe18a471541cf4f
Momtchil Momtchev <momtchil@momtchev.com>
2026-01-09 18:17:04 +0100

    add a run test

---
+++ Examples/test-suite/javascript/typescript_insert_runme.js
@@ -0,0 +1,14 @@
+var typescript_insert = require('typescript_insert');
+
+var a = 2; //  @ts-replace var a: typescript_insert.Custom = 2;
+var r = typescript_insert.add(a, 3);
+if (r !== 5) throw new Error;
+
+var pass = false;
+try {
+  // @ts-expect-error
+  typescript_insert.add('a', 'b');
+} catch {
+  pass = true;
+}
+if (!pass) throw new Error;
